d25faac1f16b6944b53bdb717f923bf2720aed6d
lvoelz_wctc <luke.voelz@gmail.com>
2020-11-11 21:22:40 -0800

    Updates after class work

--- validation.js
+++ validation.js
@@ -1,26 +1,52 @@
 $(document).ready(function () {
     // Object containing the validation rules
+
     var myRules =
-        {};
+        {
+            firstName:
+                {
+                    required: true
+                },
+            grade:
+                {
+                    required: true,
+                    min: 5,
+                    max: 8,
+                    digits: true
+                }
+        };
 
     // Object containing the error messages
     var myMessages =
-        {};
+        {
+            firstName:
+                {
+                    required: "First Name is required."
+                },
+            grade:
+                {
+                    required: "Grade is required",
+                    min: "Too small",
+                    max: "Too big",
+                    digits: "Must be numbers"
+                }
 
-    // Pass the configuration to the form's validate() method
-    // Needs submitHandler, rules, and messages properties
+        };
+
     $("form").validate(
-        {}
+        {
+            submitHandler: runMyProgram,
+            rules: myRules,
+            messages: myMessages,
+        }
     );
 
     function runMyProgram() {
-        // Change the text of the <p> with ID of "message" to
-        // state, for example, "You have registered Jane for grade 6 camp!"
-        // Use the name and grade the user provided in the form.
+        $("form").submit(runMyProgram);
+        var firstName = $("input[name=firstName]").val();
+        var grade = $("input[name=grade]").val();
 
-
+        $("#message").text("You have registered " + firstName + " for grade " + grade + " camp!");
     }
-
-
 
 });
